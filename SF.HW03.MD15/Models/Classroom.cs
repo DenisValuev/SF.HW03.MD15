463d1888859bf2f958665da72f916899fe3b3594
DenisValuev <valuev.denis91@mail.ru>
2024-10-25 12:07:19 +0300

    Добавьте файлы проекта.

---
+++ SF.HW03.MD15/Models/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF.HW03.MD15.Models
+{
+    internal class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ SF.HW03.MD15/Program.cs
@@ -0,0 +1,28 @@
+﻿using SF.HW03.MD15.Models;
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+                new Classroom { Students = { "Evgeniy", "Sergey", "Andrew"} },
+                new Classroom { Students = { "Anna", "Viktor", "Vladimir" } },
+                new Classroom { Students = { "Bulat", "Alex", "Galina" } },
+            };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        /// <summary>
+        /// Собирает всех учеников классов в один список
+        /// </summary>
+        /// <param name="classes"></param>
+        /// <returns></returns>
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(c => c.Students).ToArray();
+        }
+    }
